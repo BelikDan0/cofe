0de5f4dce21453a55a4f545e9dbd759c2cad3c3b
106010190 <belikdana85@gmail.com>
2025-04-25 14:29:27 +0300

    cofe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Drink firstDrink=new Tee("чай",0.5,80,true,true);
         Drink twiseDrink=new Latte("Latte",0.5,150,true,true);
+        Drink threeDrink=new Americano("Americano",0.8,100,true,true);
         System.out.println(firstDrink.getTOtalPrise());
         System.out.println(twiseDrink.getTOtalPrise());
+        System.out.println(threeDrink.getTOtalPrise());
     }
 }
